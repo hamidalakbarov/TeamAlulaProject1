11fa9104d0866385fee52e61fcc9c72e919f98c4
Hamid <hamidalakbarov@gmail.com>
2023-01-05 15:38:36 -0800

    changing file

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,5 +3,11 @@ public class MyClass {
     public static void main(String[] args) {
 
         System.out.println("My Class");
+
+
+
+        // this is my changes done by Hamid
+        // another change
+        // changes by me
     }
 }
